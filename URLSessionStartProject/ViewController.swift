f03d3aef020cf747cb84256b9c3d4843b6b8e5c2
Yzee <yan.zhigurs@mail.ru>
2023-03-02 19:03:05 +0400

    setting

--- URLSessionStartProject/ViewController.swift
+++ URLSessionStartProject/ViewController.swift
@@ -13,13 +13,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         executeCall()
     }
     
     func executeCall() {
         let endpoint = GetNameEndpoint()
-        let completion: EndpointClient.ObjectEndpointCompletion<String> = { result, response in
+        let completion: EndpointClient.ObjectEndpointCompletion<Cards> = { result, response in
             guard let responseUnwrapped = response else { return }
 
             print("\n\n response = \(responseUnwrapped.allHeaderFields) ;\n \(responseUnwrapped.statusCode) \n")
@@ -32,52 +31,36 @@ class ViewController: UIViewController {
                     print("Type: \(i.type ?? "")")
                     print("Artist: \(i.artist ?? "") \n")
                 }
-                
             case .failure(let error):
                 print(error)
             }
         }
-        
         endpointClient.executeRequest(endpoint, completion: completion)
     }
-
-
 }
 
-final class GetNameEndpoint: ObjectResponseEndpoint<String> {
+final class GetNameEndpoint: ObjectResponseEndpoint<Cards> {
     
     override var method: RESTClient.RequestType { return .get }
     override var path: String { "/v1/cards" }
-//    override var queryItems: [URLQueryItem(name: "id", value: "1")]?
     
     override init() {
         super.init()
 
-        queryItems = [URLQueryItem(name: "name", value: "Black Lotus")]
+        queryItems = [URLQueryItem(name: "name", value: "Opt")]
+        //queryItems = [URLQueryItem(name: "name", value: "Black Lotus")]
     }
-    
 }
-
-
-
-
-
-
-
-
-
-
 
 func decodeJSONOld() {
     let str = """
         {\"team\": [\"ios\", \"android\", \"backend\"]}
     """
-    
     let data = Data(str.utf8)
 
     do {
         if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-            if let names = json["team"] as? [String] {
+            if let names = json["cards"] as? [String] {
                 print(names)
             }
         }
@@ -85,4 +68,3 @@ func decodeJSONOld() {
         print("Failed to load: \(error.localizedDescription)")
     }
 }
-
